42f02eeff69d22d4dff0f8bb95d3f86fb501d7dd
poljkee2010 <31631937+poljkee2010@users.noreply.github.com>
2017-09-24 01:42:31 +0300

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -63,22 +63,21 @@ T stack<T>::pop()
 }
 
 int main() {
-	try 
+try 
+{
+	stack<int> st; 
+	int ct = 0;
+	char ch;
+	while (ct++<10)
 	{
-		stack<int> st; 
-		int ct = 0;
-		char ch;
-
-		while (ct++<10)
-		{
-			cin >> ch;
-			st.push(ch); // помещаем элементы в стек
-		}
+		cin >> ch;
+		st.push(ch); // помещаем элементы в стек
 	}
+}
     
-	catch (const exception& e)  	//обработка исключений
-  {
-    cout << e.what() << endl;
-	}
+catch (const exception& e)  	//обработка исключений
+{
+	cout << e.what() << endl;
+}
 	return 0;
 }
