5fc484175519ca6b76910553129bb641683eba8a
poljkee2010 <31631937+poljkee2010@users.noreply.github.com>
2017-10-04 20:35:10 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
-#include <stack.hpp>
+#include "stack.hpp"
 using namespace std;
 
 int main() 
 {
-try {
-cout << "Elements of STACK:" << endl;
+try 
+{
+        cout << "Elements of STACK:" << endl;
 
-stack<int> headStack;
-headStack.push(0);
-headStack.push(1);
-headStack.push(2);
-headStack.push(3);
-headStack.push(4);
-headStack.push(5);
-headStack.push(6);
-headStack.push(7);
-headStack.push(8);
-headStack.push(9);
+        stack<int> headStack;
+        headStack.push(0);
+        headStack.push(1);
+        headStack.push(2);
+        headStack.push(3);
+        headStack.push(4);
+        headStack.push(5);
+        headStack.push(6);
+        headStack.push(7);
+        headStack.push(8);
+        headStack.push(9);
 
-for (int i = 0; i < 10; i++) 
+        for (int i = 0; i < 10; i++) 
         {
-cout << headStack.pop() << endl;
+                cout << headStack.pop() << endl;
         }
-
-cout << "Number of elements in Stack = " << headStack.count() << endl;
-headStack.pop(); //Вызов исключения т.к. стек пустой
+        cout << "Number of elements in Stack = " << headStack.count() << endl;
+        headStack.pop(); //Вызов исключения т.к. стек пустой
 }
-catch (const exception& e) //Обработка исключений
-{ 
-cout << e.what() << endl;
-}
+        catch (const exception& e) //Обработка исключений
+        { 
+                cout << e.what() << endl;
+        }
 return 0;
 }
